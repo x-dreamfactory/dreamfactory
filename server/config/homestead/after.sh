7879f1242f786adf078e470b53198685515499b1
Ariful Islam <areef.islam@gmail.com>
2018-11-21 12:03:51 -0500

    updated homestead config script

--- server/config/homestead/after.sh
+++ server/config/homestead/after.sh
@@ -20,93 +20,21 @@ echo "postfix postfix/mailname string mail.example.com" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y postfix > $OUTPUT 2>&1
 
-echo ">>> Installing php ldap extension"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y php7.1-ldap > $OUTPUT 2>&1
-
-echo ">>> Installing php sybase extension"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y php7.1-sybase > $OUTPUT 2>&1
-
-echo ">>> Installing php interbase (firebird) extension"
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq -y php7.1-interbase > $OUTPUT 2>&1
-
 echo ">>> Installing php mongodb extension"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq -y php7.1-mongodb > $OUTPUT 2>&1
-
-echo ">>> Installing V8 and php v8js extension"
-git clone https://github.com/dreamfactorysoftware/v8-compiled.git > $OUTPUT 2>&1
-cd v8-compiled
-sudo mkdir /opt/v8 > $OUTPUT 2>&1
-sudo cp -R ubuntu_16.04/PHP7.1/* /opt/v8 > $OUTPUT 2>&1
-git clone https://github.com/phpv8/v8js.git > $OUTPUT 2>&1
-cd v8js
-phpize > $OUTPUT 2>&1
-./configure --with-v8js=/opt/v8 > $OUTPUT 2>&1
-make > $OUTPUT 2>&1
-sudo make install > $OUTPUT 2>&1
-sudo echo "extension=v8js.so" > /etc/php/7.1/mods-available/v8js.ini
-sudo phpenmod v8js > $OUTPUT 2>&1
-cd ../../
-sudo rm -Rf v8-compiled
-
-echo ">>> Installing php cassandra extension"
-mkdir cassandra
-cd cassandra
-sudo apt-get install -qq -y libgmp-dev libpcre3-dev g++ make cmake libssl-dev openssl > $OUTPUT 2>&1
-wget -q http://downloads.datastax.com/cpp-driver/ubuntu/16.04/dependencies/libuv/v1.11.0/libuv_1.11.0-1_amd64.deb
-wget -q http://downloads.datastax.com/cpp-driver/ubuntu/16.04/dependencies/libuv/v1.11.0/libuv-dev_1.11.0-1_amd64.deb
-wget -q http://downloads.datastax.com/cpp-driver/ubuntu/16.04/cassandra/v2.6.0/cassandra-cpp-driver_2.6.0-1_amd64.deb
-wget -q http://downloads.datastax.com/cpp-driver/ubuntu/16.04/cassandra/v2.6.0/cassandra-cpp-driver-dev_2.6.0-1_amd64.deb
-sudo dpkg -i libuv_1.11.0-1_amd64.deb > $OUTPUT 2>&1
-sudo dpkg -i libuv-dev_1.11.0-1_amd64.deb> $OUTPUT 2>&1
-sudo dpkg -i cassandra-cpp-driver_2.6.0-1_amd64.deb > $OUTPUT 2>&1
-sudo dpkg -i cassandra-cpp-driver-dev_2.6.0-1_amd64.deb > $OUTPUT 2>&1
-git clone https://github.com/datastax/php-driver.git > $OUTPUT 2>&1
-cd php-driver
-git checkout tags/v1.2.2 > $OUTPUT 2>&1
-cd ext
-phpize > $OUTPUT 2>&1
-./configure > $OUTPUT 2>&1
-make > $OUTPUT 2>&1
-sudo make install > $OUTPUT 2>&1
-sudo echo "extension=cassandra.so" > /etc/php/7.1/mods-available/cassandra.ini
-sudo phpenmod cassandra > $OUTPUT 2>&1
-cd ../../../
-sudo rm -R cassandra
-
-echo ">>> Installing php couchbase extension"
-wget http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-3-amd64.deb > $OUTPUT 2>&1
-sudo dpkg -i couchbase-release-1.0-3-amd64.deb > $OUTPUT 2>&1
-sudo apt-get update > $OUTPUT 2>&1
-sudo apt-get -y install libcouchbase-dev build-essential php-dev zlib1g-dev > $OUTPUT 2>&1
-sudo pecl -d php_suffix=7.1 install couchbase > $OUTPUT 2>&1
-sudo echo "extension=couchbase.so" > /etc/php/7.1/mods-available/xcouchbase.ini
-sudo php -r 'file_put_contents("/etc/php/7.1/cli/php.ini", str_replace("extension=\"couchbase.so\"", "", file_get_contents("/etc/php/7.1/cli/php.ini")));' > $OUTPUT 2>&1
-sudo phpdismod couchbase > $OUTPUT 2>&1
-sudo phpenmod xcouchbase > $OUTPUT 2>&1
 
 echo ">>> Installing phpMyAdmin (http://host/pma)"
 cd */.
 composer create-project phpmyadmin/phpmyadmin --repository-url=https://www.phpmyadmin.net/packages.json --no-dev public/pma > $OUTPUT 2>&1
 
-# echo ">>> Installing bower"
-# sudo npm install -g bower > $OUTPUT 2>&1
-# echo ">>> Installing grunt-cli"
-# sudo npm install -g grunt-cli > $OUTPUT 2>&1
-
-# echo ">>> Setting up workbench for some packages";
-# mkdir -p workbench/repos > $OUTPUT 2>&1
-# cd workbench/repos
-# echo ">>> ----> Cloning df-admin-app";
-# git clone -b develop https://github.com/dreamfactorysoftware/df-admin-app.git > $OUTPUT 2>&1
-# echo ">>> ----> Cloning df-api-docs-ui";
-# git clone -b develop https://github.com/dreamfactorysoftware/df-api-docs-ui.git > $OUTPUT 2>&1
-# echo ">>> ----> Cloning df-filemanager-app";
-# git clone -b develop https://github.com/dreamfactorysoftware/df-filemanager-app.git > $OUTPUT 2>&1
-# cd ../../public
-# ln -s ../workbench/repos/df-api-docs-ui dev-df-api-docs-ui > $OUTPUT 2>&1
-# ln -s ../workbench/repos/df-admin-app dev-df-admin-app > $OUTPUT 2>&1
-# ln -s ../workbench/repos/df-filemanager-app dev-df-filemanager-app > $OUTPUT 2>&1
-# cd ../
+echo ">>> Setting up workbench for some packages";
+mkdir -p workbench/repos > $OUTPUT 2>&1
+cd workbench/repos
+echo ">>> ----> Cloning df-user";
+git clone -b role_access_fix https://github.com/x-dreamfactory/df-user.git > $OUTPUT 2>&1
+cd ../../
+php -r 'strpos(file_get_contents("composer.json"), "User\\\\\":") === false && file_put_contents("composer.json", str_replace("\"DreamFactory\\\\\": \"app/\",", "\"DreamFactory\\\\\": \"app/\",\n      \"DreamFactory\\\\Core\\\\User\\\\\": \"workbench/repos/df-user/src/\",", file_get_contents("composer.json")));'
+composer dump-autoload > $OUTPUT 2>&1
 
 echo ">>> Installing workbench git tools"
 cp server/config/homestead/tools/*.php workbench/repos/
